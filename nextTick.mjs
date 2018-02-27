3e6fbe64c312691ccd9cc0c70d562ced6889a993
lon <longhaohe@gmail.com>
2018-02-27 18:49:20 +0800

    Update project

--- nextTick.mjs
+++ nextTick.mjs
@@ -2,48 +2,55 @@ var callbacks = []
 var pending = false
 function nextTickHandler () {
     pending = false
-    var callbackCopies = callbacks.slice(0)
-    callbacks.length = 0
-    for (var i = 0; i < callbackCopies.length; i++) {
-        callbackCopies[i]()
+    var length = callbacks.length
+    if (length === 0) {
+        return
+    }
+    var cbs = callbacks.splice(0)
+    for (var i = 0; i < length; i++) {
+        cbs[i]()
     }
 }
 
 var callNextTick
 if (typeof process === 'object'
-    && process
-    && typeof process.nextTick === 'function'
+    && process !== null
+    && typeof process['nextTick'] === 'function'
 ) {
     callNextTick = process.nextTick
 } else if (typeof MutationObserver !== 'undefined') {
-    var counter = 0
-    var textNode = document.createTextNode(counter)
+    var a = 'a'
+    var b = 'b'
+    var value = a
+    var commentNode = document.createComment(value)
     var observer = new MutationObserver(nextTickHandler)
-    observer.observe(textNode, {
+    observer.observe(commentNode, {
         characterData: true
     })
     callNextTick = function () {
-        counter = counter === 0 ? 1 : 0
-        textNode.data = String(counter)
+        value = value === a ? b : a
+        commentNode.data = value
     }
 } else {
-    callNextTick = setTimeout
+    callNextTick = function (callback) {
+        setTimeout(callback, 0)
+    }
 }
 
 
 export default function nextTick (callback, context) {
-    var func = callback
+    var cb = callback
     if (context) {
         var args = []
         var l = arguments.length
         while (--l > 1) {
             args.unshift(arguments[l])
         }
-        func = function () {
+        cb = function () {
             callback.apply(context, args)
         }
     }
-    callbacks.push(func)
+    callbacks.push(cb)
     if (pending) {
         return
     }
